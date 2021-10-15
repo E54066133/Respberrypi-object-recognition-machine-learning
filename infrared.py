c0b4300cb5fdc6eacbb63655026af3b25b11d7ab
E54066133 <45085286+E54066133@users.noreply.github.com>
2021-10-15 20:53:59 +0800

    Create infrared.py

---
+++ infrared.py
@@ -0,0 +1,22 @@
+import time
+import requests
+import RPi.GPIO as GPIO
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+lineserver_host =config.get('line-bot', 'lineserver_host') 
+SEN_PIN = 36 #IR sensor腳位
+data = {
+    "name": "Jason",
+    "SENSOR": "ON"
+}
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(SEN_PIN,GPIO.IN)
+try:
+    while True:
+        #print(GPIO.input(SEN_PIN))
+        if GPIO.input(SEN_PIN) == GPIO.HIGH:#有人接進時觸發反應
+            r = requests.get(lineserver_host+"/callback", params=data)
+        time.sleep(1)
+except Exception as e:
+    print(e)
